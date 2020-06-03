1daba71fd16a22f89ef87efcfd92ae6145ff0993
Ban-GunOo <65142485+Ban-GunOo@users.noreply.github.com>
2020-06-03 23:20:47 +0900

    Update GraphMake(수정).py

--- GraphMake(수정).py
+++ GraphMake(수정).py
@@ -11,47 +11,52 @@ pd.set_option('display.width', 1000)
 class GraphMake:
     def __init__(self):                     #데이터 선언
         self.num = 0
-        self.list = [self.Name,self.price,self.Menu]
         self.rs_list = []
         self.Money_list = []
 
-    def Add(self,name,price,menu):          #데이터 리스트에 추가
-        self.list.append = (name,price,menu)
+    def Add(self,name,price):          #데이터 리스트에 추가
 
         self.rs_list.append(name)            #식당이름 전용 리스트
         self.Money_list.append(price)         #가격대 전용 리스트
-
         self.num +=1
 
-        return self.rs_list , self.Money_list       #식당리스트 와 가격대 리스트들 리턴하기
+
 
     # 막대 그래프를 출력하는 함수
-    def Graph(self,rs_list,Money_list,Menu):    #리턴했던 리스트들과 검색한 음식이름 가져오기
+    def Graph(self,menu):    #리턴했던 리스트들과 검색한 음식이름 가져오기
         font_name = font_manager.FontProperties(fname="c:/Windows/Fonts/malgun.ttf").get_name()
         rc('font', family=font_name)  # 그래프에 한글을 출력하기 위해 필요한 문장
 
+        Money = len(self.Money_list)
+        Rs = len(self.rs_list)
         # 데이터 프레임 변환
-
-        plt.figure(figsize=(12, 8))  # 그래프의 크기 지정
-
-        pos = np.arange(self.num)
-        plt.bar(pos, Money_list, align='center', width=0.5,color = 'red')
+        plt.figure(figsize=(15, 8))  # 그래프의 크기 지정
+        if Money & Rs >= 20:                     # 식당 개수가 많을 시 맨 앞에 있는 20개로 제한
+            pos = np.arange(20)
+            xpos = np.arange(20)
+            plt.plot([1,2,3],label='최대 20개까지만 보여줍니다',color = 'red')
+            plt.legend(loc='upper right')
+        else :                                   # 20개가 안넘을 시 리스트 크기에 따라서 길이 설정
+            pos = np.arange(len(self.Money_list))
+            xpos = np.arange(len(self.rs_list))  # x축 길이
+        plt.bar(pos, self.Money_list[:20], align='center', width=0.6,color = 'red')
 
         # x축 설정
-        xpos = np.arange(len(Money_list))       #x축 길이
-        plt.xticks(xpos + 1, rs_list)
+
+        plt.xticks(xpos , self.rs_list )
         plt.xlabel("식당이름", fontsize=14)       #x축 이름과 폰트 크기
 
         # y축 설정
-        plt.yticks([10000, 20000, 30000, 40000, 50000])  # 그래프 y축 간격 10000원씩
+        plt.yticks([10000, 20000, 30000, 40000, 50000,60000])  # 그래프 y축 간격 10000원씩
         plt.ylabel("가격대", fontsize=14)        #y축 이름과 폰트크기
         plt.ylim([2000, 60000])  # 그래프 최솟값 최댓값
 
-        plt.title(Menu)             #제목 (음식이름)
-        plt.savefig('./django_test/templates/Graph.png', format='png', dpi=350)  # 그래프 저장하고 출력
+        plt.title(menu, fontsize = 20 )             #제목 (음식이름)
+        plt.savefig('./django_test/templates/Graph.png', format='png', dpi=350)  # 그래프 저장하고 출
         plt.show()
 
 #예시
-#Graph = GraphMake()
-#Graph.Add('중국집',15000,'짜장면')
-#Graph.Graph(rs_list,Money_list,Menu)
+Graph = GraphMake()
+Graph.Add("중국집",30000)
+Graph.Add("한식집",8000)
+Graph.Graph('먹을거')
